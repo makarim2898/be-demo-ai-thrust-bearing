08fdb710de3a1a68bc38d9fbee2bdff39902a435
kaizen-epd <kaizen.epd.norepy@gmail.com>
2024-08-16 16:48:53 +0700

    update data

--- simple_display.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, Response, request, jsonify
-from flask_cors import CORS
-
-simpleDisplay = Blueprint('simpleDisplay_routes', __name__)
-CORS(simpleDisplay)
-
-@simpleDisplay.route('/display/tipu', methods=['GET'])
-def tipu_index():
-    return "tipu-tipu-display"
