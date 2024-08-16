da029833392f98deb08a8546c5e9e4f9a046797b
kaizen-epd <kaizen.epd.norepy@gmail.com>
2024-08-16 15:34:35 +0700

    register sample simple_display

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from settings_function import settings
 from Home_page_trust_bearing import home_bearing
 from information import information
 from openBrowser import openBrowser
+from simple_display import simpleDisplay
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(settings)
 app.register_blueprint(home_bearing)
 app.register_blueprint(information)
+app.register_blueprint(simpleDisplay)
 
 @app.route('/')
 def index():
