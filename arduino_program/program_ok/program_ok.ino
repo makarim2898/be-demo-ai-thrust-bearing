a6b1a0394b8dd435f71d5e49e7e5a7db8c7da154
kaizen-epd <kaizen.epd.norepy@gmail.com>
2024-09-19 10:56:53 +0700

    sesuaikan flow prosesdengan kondisi arduino minima

--- arduino_program/program_ok/program_ok.ino
+++ arduino_program/program_ok/program_ok.ino
@@ -1,16 +1,19 @@
 //inisaialisasi input
-#define input_trigger 7
-#define reset_trigger 8
+#define start_trigger 12
+#define reset_trigger 11
+#define man_ok_trigger 10
 
 //inisialisasi output  
-#define out_ok 13
-#define out_ng 12
+#define ok_trigger 4
+#define out_ok 5
+#define out_ng 6
 
 void matikan_lampu(int pin_out = -1){
     // matikan semua output
     if (pin_out == -1){
         digitalWrite(out_ok, HIGH);
         digitalWrite(out_ng, HIGH);
+        digitalWrite(ok_trigger, HIGH);
       }
     else{
         digitalWrite(pin_out, HIGH);
@@ -20,11 +23,13 @@ void matikan_lampu(int pin_out = -1){
 void setup() {
   // definisi pin INPUT
   pinMode(reset_trigger, INPUT_PULLUP);
-  pinMode(input_trigger, INPUT_PULLUP);
+  pinMode(start_trigger, INPUT_PULLUP);
+  pinMode(man_ok_trigger, INPUT_PULLUP);
 
   //definisi pin output
   pinMode(out_ok, OUTPUT);
   pinMode(out_ng, OUTPUT);
+  pinMode(ok_trigger, OUTPUT);
 
   // inisialisasi serial
   Serial.begin(115200);
@@ -44,12 +49,17 @@ void loop() {
 }
 //****************** ini yang kirim serial ke python ***************************
 void read_pin_input() {
-  if (digitalRead(input_trigger) == LOW) {
+  if (digitalRead(start_trigger) == LOW) {
     Serial.println("start_scan");
     delay(100);
   } 
   else if(digitalRead(reset_trigger) == LOW){
     Serial.println("reset_scan");
+    matikan_lampu();
+    delay(100);
+  }
+  else if(digitalRead(man_ok_trigger) == LOW){
+    Serial.println("manual_ok");
     matikan_lampu();
     delay(100);
   }
@@ -67,9 +77,15 @@ void read_data_python() {
       String data = Serial.readString();
       if(data == "out_ok"){
         digitalWrite(out_ok, LOW);
+        digitalWrite(ok_trigger, LOW);
+        digitalWrite(out_ng, HIGH);
+        delay(2000);
         }
       else if(data == "out_ng"){
+        digitalWrite(out_ok, HIGH);
         blink_output(out_ng, 3, 5);
+        digitalWrite(ok_trigger, HIGH);
+
         }
       // Mengembalikan data yang diterima ke port serial
       Serial.print("Pesan yang diterima arduino: ");
