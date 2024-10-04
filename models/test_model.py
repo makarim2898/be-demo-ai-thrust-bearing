c7c4ca50a493bc4f910586070d4660a97db61314
kaizen-epd <kaizen.epd.norepy@gmail.com>
2024-10-04 14:45:33 +0700

    easily modsel path

--- models/test_model.py
+++ models/test_model.py
@@ -3,10 +3,17 @@ import cv2
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO("model6.pt")
+model = YOLO("/home/kaizen-ai/Desktop/thrust_bearing_app/python-backend/models/model1yolov10n.pt")
 
 # Open the video file
 cap = cv2.VideoCapture(0)
+
+frame_width = 480
+frame_height = 240
+
+frame_width = int((frame_height / 9) * 16)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
 # Loop through the video frames
 while cap.isOpened():
@@ -15,7 +22,7 @@ while cap.isOpened():
 
     if success:
         # Run YOLOv8 inference on the frame
-        results = model(frame, conf=0.8)
+        results = model(frame, conf=0.6, max_det=2)
 
         # Visualize the results on the frame
         annotated_frame = results[0].plot()
