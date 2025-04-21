d8deb7d83223b50144255145e9b2d644f52c00d1
kaizen-epd <kaizen.epd.norepy@gmail.com>
2025-04-21 10:19:16 +0700

    update test model

--- models/test_model.py
+++ models/test_model.py
@@ -3,7 +3,7 @@ import cv2
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO("/home/komputer/Desktop/thrust_bearing_app/python-backend/models/best.pt")
+model = YOLO("./models/yolo11 bearing AI.pt")
 
 # Open the video file
 cap = cv2.VideoCapture(0)
